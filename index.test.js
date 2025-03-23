64987ff7aeef53b3a4f2e9cba83c9e9749a80221
= <=>
2025-03-23 19:14:16 +0530

    added new line test

--- index.test.js
+++ index.test.js
@@ -32,3 +32,10 @@ test('throws an error for negative numbers', () => {
     expect(() => add("-1,-2,-3")).toThrow("Negative numbers not allowed: -1, -2, -3");
     expect(() => add("-10,5")).toThrow("Negative numbers not allowed: -10");
 });
+
+
+test('handles new line as a delimiter', () => {
+    expect(add("1\n2,3")).toBe(6);
+    expect(add("1\n2\n3")).toBe(6);
+    expect(add("1,2\n3,4")).toBe(10);
+});
